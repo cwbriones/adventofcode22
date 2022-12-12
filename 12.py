4a6f7b1b15122560f3552998e16ebe8d163be2f7
Christian Briones <cwbriones@gmail.com>
2022-12-12 12:32:26 -0800

    networkx

--- 12.py
+++ 12.py
@@ -1,30 +1,26 @@
 import sys
-import copy
-from search import ucs
+import networkx
+from collections import defaultdict
 
 
-def one(grid):
-    start = find_replace(grid, "S", "a")
-    assert start is not None
-    end = find_replace(grid, "E", "z")
-    assert end is not None
+def to_adj(
+    grid,
+):
+    adj = defaultdict(list)
+    for i, j, _ in enumerate_grid(grid):
+        for n in neighbors(grid, (i, j)):
+            adj[(i, j)].append(n)
+    return adj
 
-    pathlen = ucs(grid, start, end, connected)
-    assert pathlen is not None
-    print(pathlen)
 
-
-def two(grid):
-    find_replace(grid, "S", "a")
-    end = find_replace(grid, "E", "z")
-    assert end is not None
-
-    shortest = min(
-        pathlen
-        for start in starts(grid, "a")
-        if (pathlen := ucs(grid, start, end, connected)) is not None
-    )
-    print(shortest)
+def neighbors(grid, p):
+    px, py = p
+    for nx, ny in [(px - 1, py), (px + 1, py), (px, py - 1), (px, py + 1)]:
+        if nx < 0 or nx >= len(grid[0]) or ny < 0 or ny >= len(grid):
+            continue
+        if ord(grid[ny][nx]) - ord(grid[py][px]) > 1:
+            continue
+        yield nx, ny
 
 
 def find_replace(grid, target, replace):
@@ -36,24 +32,32 @@ def find_replace(grid, target, replace):
     return None
 
 
-def starts(grid, target):
+def enumerate_grid(grid):
     for j, row in enumerate(grid):
         for i, c in enumerate(row):
-            if c == target:
-                yield (i, j)
-
-
-def connected(h1, h2):
-    return ord(h2) - ord(h1) <= 1
+            yield i, j, c
 
 
 def input():
     return [list(line.strip()) for line in sys.stdin.readlines()]
 
 
-def main(lines):
-    one(copy.deepcopy(lines))
-    two(lines)
+def main(grid):
+    start = find_replace(grid, "S", "a")
+    end = find_replace(grid, "E", "z")
+    assert end is not None
+
+    adj = to_adj(grid)
+    graph = networkx.DiGraph(incoming_graph_data=adj)
+
+    pathlens = networkx.shortest_path_length(graph, target=end)
+    # one
+    print(pathlens[start])
+
+    # two
+    starts = ((i, j) for i, j, c in enumerate_grid(grid) if c == "a")
+    pathlen = min(pathlens[start] for start in starts if start in pathlens)
+    print(pathlen)
 
 
 if __name__ == "__main__":
