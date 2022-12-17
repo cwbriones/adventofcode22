8abca7f9c52eb6e6207b9c1af9ecadfa473aa2b4
Christian Briones <cwbriones@gmail.com>
2022-12-17 00:34:54 -0800

    much better solutions for 15 and 16

--- 15.py
+++ 15.py
@@ -1,102 +1,47 @@
-import argparse
-from collections import (
-    defaultdict,
-    deque,
-)
-from itertools import chain
-from contextlib import contextmanager
-from dataclasses import dataclass
-import functools
-import math
-import os
 import sys
-from copy import deepcopy
 import re
 
-PAT = re.compile(r"at x=(-?\d+), y=(-?\d+)")
+Point = tuple[int, int]
 
 
-def manhattan(p, q):
+def one(beacons, ycheck):
+    """
+    For part one, we take all scanner/beacon pairs. Knowing the y coordinate to
+    check, we keep the x-delta fixed such that the manhattan distance is no closer
+    than that sensors beacon (otherwise it would be the closest). This gives us
+    an interval where the beacon *definitely isn't*.
+
+    Once we have these intervals, they are sorted and collapsed into a set of
+    non-overlapping intervals. We can then simply sum the lengths of all of these
+    intervals for the answer.
+    """
+    best = {s: manhattan(s, beacons[s]) for s in beacons}
+    in_row = sum(1 for _, by in set(sensors.values()) if by == ycheck)
+    ranges = scan_fixed_y(best, ycheck)
+    not_possible = sum(b - a + 1 for (a, b) in ranges) - in_row
+    print(not_possible)
+
+
+def manhattan(p: Point, q: Point) -> int:
     return abs(p[1] - q[1]) + abs(p[0] - q[0])
 
-def scan_fixed_x(sensors, check):
-    return scan_fixed(sensors, check, selector=lambda p: p)
 
 def scan_fixed_y(sensors, check):
-    return scan_fixed(sensors, check, selector=lambda p: (p[1], p[0]))
-
-def scan_fixed(sensors, check, selector):
     ranges = []
     for sensor, best in sensors.items():
-        fst, snd = selector(sensor)
-        delta = best - abs(fst - check)
-        if delta < 0:
+        x, y = sensor
+        deltax = best - abs(y - check)
+        if deltax < 0:
             # we can't learn anything from this sensor
             continue
         # anything at most deltax away cannot possibly have a beacon
-        start, end = snd - delta, snd + delta
+        start, end = x - deltax, x + deltax
         if start > end:
             raise ValueError("wat")
         ranges.append((start, end))
     ranges.sort(key=lambda p: p[0])
     return merge(ranges)
 
-def complement(ranges, low, high):
-    possible = []
-    lastend = low
-    for start, end in ranges:
-        if start > lastend:
-            possible.append((lastend, start - 1))
-        lastend = end + 1
-    if lastend < high:
-        possible.append((lastend, high))
-    return possible
-
-def one(sensors, ycheck):
-    best = {s: manhattan(s, sensors[s]) for s in sensors}
-    in_row = sum(
-        1
-        for _, by in set(sensors.values())
-        if by == ycheck
-    )
-    ranges = scan_fixed_y(best, ycheck)
-    not_possible = sum(b - a + 1 for (a, b) in ranges) - in_row
-    print(not_possible)
-
-def two(sensors, ycheck):
-    best = {s: manhattan(s, sensors[s]) for s in sensors}
-    score = lambda y: sum(abs(s[1] - y) for s in best if abs(s[1] - y) > 0)
-
-    # find the min score in the y direction
-    minscore = float('inf')
-    lo, hi = 0, ycheck*2
-    while lo < hi:
-        mid = (lo + hi) // 2
-        if score(mid) < score(mid + 1):
-            hi = mid
-        else:
-            lo = mid + 1
-    y = lo
-
-    while True:
-        curscore = score(y-1)
-        if curscore <= minscore:
-            minscore = curscore
-            y -= 1
-        else:
-            break
-
-    # search the space from here onward
-    while y < ycheck*2:
-        # print(f'==== y={y} ====')
-        ranges = scan_fixed_y(best, y)
-        possible = complement(ranges, 0, ycheck*2)
-        if possible:
-            x = possible[0][0]
-            print(f'x={x} y={y} {score(y)}')
-            print(4000000*x + y)
-            break
-        y += 1
 
 def merge(ranges):
     assert len(ranges) > 0
@@ -108,24 +53,100 @@ def merge(ranges):
         merged.append([start, end])
     return merged
 
-def dims(coords) -> tuple[int, int, int, int] | None:
-    coorditer = iter(coords)
-    try:
-        minx, miny = next(coorditer)
-        maxx, maxy = minx, miny
-        for x, y in coorditer:
-            minx = min(x, minx)
-            miny = min(y, miny)
-            maxx = max(x, maxx)
-            maxy = max(y, maxy)
-    except StopIteration:
+
+def two(beacons: dict[tuple[int, int], tuple[int, int]]):
+    """
+    For part two, we exploit the fact that the solution is unique.
+
+    This means the position is nestled between two pairs of sensors. If you
+    think of all the spaces less than or equal to each sensors best manhattan
+    distance, you get a diamond. Each diamond side has slope 1 or -1.
+
+    Since we know the point is between each pair, one of the pairs consists
+    of two sides of these diamonds with slope 1, the other pair with slope -1.
+
+    We iterate through all pairs of sensors, checking if their sides are apart
+    by exactly 2 spaces. If so, we keep that pair in a set of m=-1 and m=1.
+
+    Finally, we check all of these lines for an intersection - there should be
+    exactly one that fulfills the criteria of not being seen by any sensor.
+    """
+    sensors = {s: manhattan(s, beacons[s]) for s in beacons}
+
+    matches_up = set()
+    matches_down = set()
+    for sensora, sensorb in pairs(list(sensors)):
+        a1, b1, c1, d1 = diamonds(sensora, sensors[sensora])
+        a2, b2, c2, d2 = diamonds(sensorb, sensors[sensorb])
+        if (m := midpoint(a1, b2)) is not None:
+            matches_up.add(m)
+        elif (m := midpoint(b1, a2)) is not None:
+            matches_up.add(m)
+        elif (m := midpoint(c1, d2)) is not None:
+            matches_down.add(m)
+        elif (m := midpoint(d1, c2)) is not None:
+            matches_down.add(m)
+    candidates = intersections(matches_up, matches_down)
+    points = [
+        p for p in candidates if all(manhattan(s, p) > b for s, b in sensors.items())
+    ]
+    if len(points) != 1:
+        print("no solution found? possibly bad input")
+        return
+    ((x, y),) = points
+    print(x * 4000000 + y)
+
+
+def diamonds(sensor: Point, best: int):
+    #      / \
+    #   a /   \ c
+    #    /  .  \
+    #    \     /
+    #   d \   / b
+    #      \ /
+    a = y_intercept(x=sensor[0], y=sensor[1] + best)
+    b = y_intercept(x=sensor[0], y=sensor[1] - best)
+    c = y_intercept(x=sensor[0], y=sensor[1] + best, m=-1)
+    d = y_intercept(x=sensor[0], y=sensor[1] - best, m=-1)
+    return a, b, c, d
+
+
+def midpoint(a, b):
+    if abs(a - b) != 2:
         return None
-    return minx, maxx, miny, maxy
+    elif a < b:
+        return a + 1
+    else:
+        return b + 1
+
+
+def y_intercept(x, y, m=1):
+    return y - x * m
+
+
+def pairs(items):
+    for i, item in enumerate(items):
+        for other in items[i + 1 :]:
+            yield item, other
+
+
+def intersections(lines_up, lines_down):
+    # find where x1 == x2 and y1 == y2
+    # y =   x + up
+    # y = - x + down
+    # y = (up + down) / 2
+    for up in lines_up:
+        for down in lines_down:
+            y = (up + down) // 2
+            x = y - up
+            yield (x, y)
 
 
 def input():
-    lines = [l.strip() for l in sys.stdin.readlines()]
+    PAT = re.compile(r"at x=(-?\d+), y=(-?\d+)")
     ycheck = 2_000_000
+
+    lines = [l.strip() for l in sys.stdin.readlines()]
     try:
         ycheck = int(lines[0])
         lines = lines[1:]
@@ -135,8 +156,7 @@ def input():
     sensors = {}
     for line in lines:
         sensor, beacon = tuple(
-            (int(m.group(1)), int(m.group(2)))
-            for m in PAT.finditer(line.strip())
+            (int(m.group(1)), int(m.group(2))) for m in PAT.finditer(line.strip())
         )
         sensors[sensor] = beacon
     return sensors, ycheck
@@ -144,7 +164,7 @@ def input():
 
 def main(sensors, ycheck):
     one(sensors, ycheck)
-    two(sensors, ycheck)
+    two(sensors)
 
 
 if __name__ == "__main__":
--- 16.py
+++ 16.py
@@ -8,15 +8,13 @@ import networkx
 import typing
 import itertools
 
-from  multiprocessing import Pool, cpu_count
-
-PAT = re.compile(r"Valve ([A-Z]{2}) has flow rate=(\d+); tunnels? leads? to valves? ([A-Z]{2}(, [A-Z]{2})*)")
 
 @dataclass
 class Valve:
     name: str
     flow: int
     outs: dict[str, int]
+
 
 class SearchState(typing.NamedTuple):
     cur: Valve
@@ -29,91 +27,124 @@ class SearchState(typing.NamedTuple):
         return self._replace(
             cur=valve,
             flow=self.flow + valve.flow,
-            total=self.total + tick*self.flow,
+            total=self.total + tick * self.flow,
             n=self.n - tick,
         )
 
-def search(state: SearchState, valves, remaining, path=None):
-    if path is None:
-        path = []
-    outs = state.cur.outs
-    reachable = [r for r in outs if r in remaining and outs[r] <= state.n]
-    if not reachable:
-        # wait out remaining time
-        return state.total + state.flow * state.n, path
-    best = 0
-    bestpath = path
-    for r in reachable:
-        remaining.remove(r)
-        newstate = state.move_to(valves[r])
-        score, fullpath = search(newstate, valves, remaining, path=path + [valves[r].name])
-        if score > best:
-            best = score
-            bestpath = fullpath
-        remaining.add(r)
-    return best, bestpath
 
 def one(valves: dict[str, Valve]):
-    remaining = {name for name in valves if name != 'AA'}
-    best, _ = search(SearchState(cur=valves['AA'], n=30), valves, remaining)
+    memo = defaultdict(lambda: 0)
+    best = search(SearchState(cur=valves["AA"], n=30), valves, memo)
     print(best)
 
-def two(valves: dict[str, Valve]):
-    nonempty = set(name for name, v in valves.items() if v.flow > 0)
-    def search_with_elephant(s):
-        ele = set(s)
-        remaining = nonempty - ele
 
-        # elephant goes for ele, I go for nonempty
-        state = SearchState(cur=valves['AA'], n=26)
-        return \
-            search(state, valves, remaining)[0] +\
-            search(state, valves, ele)[0]
-    print(max(search_with_elephant(s) for s in progress(subsets(nonempty))))
+def search(state: SearchState, valves, memo, visited=None):
+    if visited is None:
+        visited = set()
+    outs = state.cur.outs
+    reachable = [
+        r for r in outs if r not in visited and outs[r] <= state.n and r != "AA"
+    ]
+
+    score = state.total + state.flow * state.n
+    key = frozenset(visited)
+    memo[key] = max(memo[key], score)
+
+    if not reachable:
+        # wait out remaining time
+        return score
+    best = 0
+    for r in reachable:
+        visited.add(r)
+        newstate = state.move_to(valves[r])
+        score = search(newstate, valves, memo, visited)
+        if score > best:
+            best = score
+        visited.remove(r)
+    return best
+
+
+def two(valves: dict[str, Valve]):
+    memo = defaultdict(lambda: 0)
+    search(SearchState(cur=valves["AA"], n=26), valves, memo)
+
+    needs_visit = frozenset(set(valves) - {"AA"})
+    fill_memo(needs_visit, memo)
+
+    best = 0
+    for subset in subsets(needs_visit):
+        me = frozenset(subset)
+        ele = needs_visit - me
+        if (total := memo[me] + memo[ele]) > best:
+            best = total
+    print(best)
+
+
+def fill_memo(visited, memo):
+    """
+    Fill in the memo with partial results for impossible final states.
+
+    Certain visited states never arise because it is impossible for the walk to
+    reach all of those nodes in the allotted time. Consider the graph
+
+                          AA <- 20 -> BB <- 15 -> CC
+
+    {'BB', 'CC'} will never be in the search results because it would take t=35
+    seconds to occur, which is greater than the t=26 limit.
+
+    This means that the best score you can get from a run that attempts to visit those
+    nodes is the best /possible/ score out of its subsets.
+
+    e.g. Subsets are {'BB'} and {'CC'}. BB is reachable in 20, CC is never reachable.
+    Therefore we expect memo[{'BB'}] to set memo[{'BB', 'CC'}] = memo[{'BB'}]
+    """
+    if visited not in memo:
+        best = 0
+        for v in visited:
+            flow = fill_memo(visited - {v}, memo)
+            if flow > best:
+                best = flow
+        memo[visited] = best
+    return memo[visited]
+
 
 def subsets(S):
     for m in range(len(S) // 2 + 1):
         yield from itertools.combinations(S, m)
 
-def progress(it):
-    items = list(it)
-    total = len(items)
-    for i, item in enumerate(items):
-        remain = total - i
-        if remain % 500 == 0:
-            print(remain)
-        yield item
 
 def input():
-    lines = [line.strip() for line in sys.stdin.readlines()]
+    PAT = re.compile(
+        r"Valve ([A-Z]{2}) has flow rate=(\d+); tunnels? leads? to valves? ([A-Z]{2}(, [A-Z]{2})*)"
+    )
 
     rates = {}
     edges = {}
-    for line in lines:
+    for line in sys.stdin.readlines():
         match = PAT.match(line)
         if match is None:
             raise ValueError(line)
         name, flow, outs, _ = match.groups()
         flow = int(flow)
         rates[name] = int(flow)
-        edges[name] = outs.split(', ')
+        edges[name] = outs.split(", ")
 
     weights = build_weights(edges, rates)
     valves = {
-        name: Valve(name=name, flow=rates[name], outs=weights[name])
-        for name in weights
+        name: Valve(name=name, flow=rates[name], outs=weights[name]) for name in weights
     }
     return valves
 
+
 def build_weights(edges: dict[str, list[str]], rates: dict[str, int]):
     graph = networkx.DiGraph(incoming_graph_data=edges)
-    nonempty = [name for name in edges if rates[name] > 0 or name == 'AA']
+    nonempty = [name for name in edges if rates[name] > 0 or name == "AA"]
 
     weights = defaultdict(dict)
     for i, end in enumerate(nonempty):
         pathlens = networkx.shortest_path_length(graph, target=end)
         assert isinstance(pathlens, dict)
-        for start in nonempty[i+1:]:
+        for start in nonempty[i + 1 :]:
             # +1 because this is time to reach and then open
             weights[start][end] = pathlens[start] + 1
             weights[end][start] = pathlens[start] + 1
